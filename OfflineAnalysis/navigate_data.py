f6b419373873a6f00f806d2182f6692e732bc00c
JuliaLieb <julia.liebmann@gmx.at>
2024-04-22 17:00:50 +0200

    finding mat and selection files

--- OfflineAnalysis/navigate_data.py
+++ OfflineAnalysis/navigate_data.py
@@ -2,7 +2,7 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import TwoSlopeNorm
+#from matplotlib.colors import TwoSlopeNorm
 import mne
 from mne.time_frequency import tfr_multitaper
 from mne.stats import permutation_cluster_1samp_test as pcluster_test
@@ -19,12 +19,13 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     data_path = cwd + '/../SubjectDataSorted/'
 
+    ''' Comment out if all .mat exist
+    # Transform all xdf files to mat files for the given config files in the Subject-Session-folder
     # ----- Define Subject and Session ----
-    subject_id = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S14', 'S15']
+    subject_id = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S14', 'S15', 'S16', 'S17']
     session_id = [0, 1, 2]
     # -------------------------------------
-
-    # Transform all xdf files to mat files for the given config files in the Subject-Session-folder
+    
     for current_subject in subject_id:
         for current_session in session_id:
             subject_directory = data_path + current_subject + '-ses' + str(current_session) + '/'
@@ -34,5 +35,33 @@ if __name__ == "__main__":
                 with open(current_config_file) as json_file:
                     config = json.load(json_file)
                 xdf_to_mat.xdf_to_mat_new(config, data_path)
+    '''
 
-    # Show raw eeg data
+    # Get .mat files per session
+    #'''
+    subject_id = 'S14'
+    session_id = 1
+    runs = [1,2,3,4,5]
+
+    subject_data_path = data_path + subject_id + '-ses' + str(session_id) + '/'
+
+    all_mat_files = []
+
+    for r in runs:
+        for file in os.listdir(subject_data_path):
+            if file.startswith(subject_id + '_run' + str(r)):
+                if file.endswith('.mat'):
+                    all_mat_files.append(file)
+    #'''
+
+    # Get specific run
+    run_id = 2
+    current_mat = [i for i in all_mat_files if i.startswith(subject_id + '_run' + str(run_id))]
+
+    # Print raw EEG
+
+
+
+
+
+
